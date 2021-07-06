417f6386f6bcfaecc1003785b2e8c07bd3531eda
caiolauro2020 <74771715+caiolauro2020@users.noreply.github.com>
2021-07-06 20:17:54 -0300

    adding xlsx version

--- csv_concat.py
+++ csv_concat.py
@@ -10,3 +10,20 @@ all_filenames = [i for i in glob.glob(f"*{file_extension}")]
 df = pd.concat([pd.read_csv(file, delimiter= ';') for file in all_filenames])
 #create excel file with created dataframe
 df.to_excel(r'insert.xlsx_path')
+
+
+#xlsx version
+
+import os
+import glob
+import pandas as pd
+dir_path = r'insert_directory_path'
+os.chdir(dir_path)
+file_extension = ".xlsx"
+#list al files contained in given directory
+all_filenames = [i for i in glob.glob(f"*{file_extension}")]
+#concatenate all files in a single dataframe
+df = pd.concat([pd.read_excel(file) for file in all_filenames])
+#create excel file with created dataframe
+df.to_excel(r'insert.xlsx_path')
+
